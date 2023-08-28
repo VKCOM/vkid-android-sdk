48384b335f1825f43da233632c191983c726a45d
Sergey Golovin <s.golovin@vk.team>
2023-08-28 15:54:54 +0300

    VKIDSDK-157: Add binary compatibility validator and first api dump. In future we should add ./gradlew :vkid:apiCheck command to release build pipeline checks

--- vkid/build.gradle.kts
+++ vkid/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("vkid.android.library")
     id("vkid.android.library.publish")
     id("org.jetbrains.kotlin.plugin.parcelize")
+    id("org.jetbrains.kotlinx.binary-compatibility-validator")
 }
 
 android {
