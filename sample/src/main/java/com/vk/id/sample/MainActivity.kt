26d06c7effd60317feeadc777b5133e01eaf07a0
Sergey Golovin <s.golovin@vk.team>
2023-10-12 13:54:42 +0300

    VKIDSDK-63: One tap public api and auth

--- sample/src/main/java/com/vk/id/sample/MainActivity.kt
+++ sample/src/main/java/com/vk/id/sample/MainActivity.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -21,33 +19,8 @@ import com.vk.id.VKID
 import com.vk.id.VKIDAuthFail
 import com.vk.id.onetap.compose.VKIDButton
 import com.vk.id.onetap.compose.VKIDButtonSmall
-import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainActivity : ComponentActivity() {
-
-    // todo move to viewmodel
-    private val vkid = VKID(this)
-
-    private val vkAuthCallback = object : VKID.AuthCallback {
-        override fun onSuccess(accessToken: AccessToken) {
-            setUiStateVkAuthComplete()
-            val token = accessToken.token.hideLastCharacters(TOKEN_VISIBLE_CHARACTERS)
-            showToast("There is token: $token")
-        }
-
-        override fun onFail(fail: VKIDAuthFail) {
-            setUiStateVkAuthComplete()
-            when (fail) {
-                is VKIDAuthFail.Canceled -> {
-                    showToast("Auth canceled")
-                }
-
-                else -> {
-                    showToast("Something wrong: ${fail.description}")
-                }
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,26 +33,35 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                val vkInProgress by vkAuthInProgress.collectAsState()
                 VKIDButton(
-                    inProgress = vkInProgress,
-                    enabled = vkInProgress.not(),
-                    onClick = ::startAuth,
-                    modifier = Modifier.width(335.dp)
+                    modifier = Modifier.width(335.dp),
+                    onAuth = ::onVKIDAuthSuccess,
+                    onFail = ::onVKIDAuthFail
                 )
                 Spacer(modifier = Modifier.height(24.dp))
                 VKIDButtonSmall(
-                    inProgress = vkInProgress,
-                    enabled = vkInProgress.not(),
-                    onClick = ::startAuth
+                    onAuth = ::onVKIDAuthSuccess,
+                    onFail = ::onVKIDAuthFail
                 )
             }
         }
     }
 
-    private fun startAuth() {
-        setUiStateVkAuthInProgress()
-        vkid.authorize(this@MainActivity, vkAuthCallback)
+    private fun onVKIDAuthSuccess(accessToken: AccessToken) {
+        val token = accessToken.token.hideLastCharacters(TOKEN_VISIBLE_CHARACTERS)
+        showToast("There is token: $token")
+    }
+
+    private fun onVKIDAuthFail(fail: VKIDAuthFail) {
+        when (fail) {
+            is VKIDAuthFail.Canceled -> {
+                showToast("Auth canceled")
+            }
+
+            else -> {
+                showToast("Something wrong: ${fail.description}")
+            }
+        }
     }
 
     private fun String.hideLastCharacters(firstCharactersToKeepVisible: Int): String {
@@ -90,14 +72,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun setUiStateVkAuthComplete() {
-        vkAuthInProgress.tryEmit(false)
-    }
-
-    private fun setUiStateVkAuthInProgress() {
-        vkAuthInProgress.tryEmit(true)
-    }
-
     private fun showToast(text: String) {
         toastOnScreen?.cancel()
         toastOnScreen = Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG)
@@ -105,7 +79,6 @@ class MainActivity : ComponentActivity() {
     }
 
     private var toastOnScreen: Toast? = null
-    private val vkAuthInProgress: MutableStateFlow<Boolean> = MutableStateFlow(false)
 
     private companion object {
         const val TOKEN_VISIBLE_CHARACTERS = 10
