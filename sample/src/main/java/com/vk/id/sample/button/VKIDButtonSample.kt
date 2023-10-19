a177eeefdd5628b05ce3a721d554c7456521a5b0
Daniil Klimchuk <daniil.klimchuk@vk.team>
2023-10-19 12:36:00 +0300

    VKIDSDK-230: Sing release apk for sample

--- sample/src/main/java/com/vk/id/sample/button/VKIDButtonSample.kt
+++ sample/src/main/java/com/vk/id/sample/button/VKIDButtonSample.kt
@@ -2,6 +2,7 @@ package com.vk.id.sample.button
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,8 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,14 +36,15 @@ fun CreateButtonsSample(
     onSuccess: (AccessToken) -> Unit,
     onFail: (VKIDAuthFail) -> Unit = {},
 ) {
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
             .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        items(buttonsData) {
+        buttonsData.forEach {
             when (it) {
                 is ListItem.Spacer -> Spacer(
                     modifier = Modifier
