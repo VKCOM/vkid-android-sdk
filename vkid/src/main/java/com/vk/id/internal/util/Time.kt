26d5b86ce1e9d7f6780f83ba37740865bca99a43
Daniil Klimchuk <daniil.klimchuk@vk.team>
2023-10-30 12:15:06 +0300

    VKIDSDK-382: Add logging for no internet error

--- vkid/src/main/java/com/vk/id/internal/util/Time.kt
+++ vkid/src/main/java/com/vk/id/internal/util/Time.kt
@@ -2,12 +2,16 @@ package com.vk.id.internal.util
 
 import android.os.Build
 import android.os.SystemClock
+import com.vk.id.internal.log.VKIDLog
 
+@Suppress("TooGenericExceptionCaught")
 internal fun currentTime(): Long {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         try {
             SystemClock.currentNetworkTimeClock().millis()
-        } catch (_: RuntimeException) {
+        } catch (exception: RuntimeException) {
+            VKIDLog.createLoggerForTag("com.vk.id.internal.util.currentTime")
+                .error("Most likely device was started without access to internet", exception)
             System.currentTimeMillis()
         }
     } else {
