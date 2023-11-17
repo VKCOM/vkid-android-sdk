341fb0332695432ab7b40a847c02a15d8a1b4182
Daniil Klimchuk <daniil.klimchuk@vk.team>
2023-11-17 07:51:16 +0300

    VKIDSDK-464: Add multibranding styling to sample

--- sample/src/main/java/com/vk/id/sample/screen/multibranding/data/MultibrandingSampleData.kt
+++ sample/src/main/java/com/vk/id/sample/screen/multibranding/data/MultibrandingSampleData.kt
@@ -2,6 +2,8 @@ package com.vk.id.sample.screen.multibranding.data
 
 import com.vk.id.multibranding.OAuth
 import com.vk.id.multibranding.OAuthListWidgetStyle
+import com.vk.id.multibranding.WidgetCornersStyle
+import com.vk.id.multibranding.WidgetSizeStyle
 import com.vk.id.sample.screen.multibranding.item.OAuthListWidgetItem
 import com.vk.id.sample.uikit.spacer.HalfSpacerItem
 import com.vk.id.sample.uikit.spacer.SpacerItem
@@ -12,7 +14,6 @@ internal val multibrandingSampleData = listOf(
     HalfSpacerItem(),
     OAuthListWidgetItem(
         style = OAuthListWidgetStyle.Dark(),
-        filter = { true }
     ),
     SpacerItem(),
     OAuthListWidgetItem(
@@ -23,6 +24,113 @@ internal val multibrandingSampleData = listOf(
     OAuthListWidgetItem(
         style = OAuthListWidgetStyle.Dark(),
         filter = { it == OAuth.VK }
+    ),
+    HalfSpacerItem(),
+    TitleItem("Light widget"),
+    HalfSpacerItem(isDarkBackground = true),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Light(),
+        isDarkBackground = true
+    ),
+    HalfSpacerItem(isDarkBackground = true),
+    TitleItem("Corner radius"),
+    HalfSpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            cornersStyle = WidgetCornersStyle.None
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            cornersStyle = WidgetCornersStyle.Rounded
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            cornersStyle = WidgetCornersStyle.Round
+        ),
+    ),
+    HalfSpacerItem(),
+    TitleItem("Sizes"),
+    HalfSpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.SMALL_32
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.SMALL_34
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.SMALL_36
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.SMALL_38
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.MEDIUM_40
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.MEDIUM_42
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.MEDIUM_44
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.MEDIUM_46
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.LARGE_48
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.LARGE_50
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.LARGE_52
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.LARGE_54
+        ),
+    ),
+    SpacerItem(),
+    OAuthListWidgetItem(
+        style = OAuthListWidgetStyle.Dark(
+            sizeStyle = WidgetSizeStyle.LARGE_56
+        ),
     ),
     HalfSpacerItem(),
 )
