b43db1e9e4fff28c34c2b3dfbbfe7588e12d2db0
Daniil Klimchuk <daniil.klimchuk@vk.team>
2023-11-24 11:14:37 +0300

    VKIDSDK-497: Handle errors in multibranding

--- sample/app/src/main/java/com/vk/id/sample/app/screen/styling/item/IconButtonItem.kt
+++ sample/app/src/main/java/com/vk/id/sample/app/screen/styling/item/IconButtonItem.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import com.vk.id.onetap.compose.onetap.OneTap
 import com.vk.id.onetap.compose.onetap.OneTapStyle
-import com.vk.id.sample.app.screen.styling.util.onVKIDAuthFail
-import com.vk.id.sample.app.screen.styling.util.onVKIDAuthSuccess
+import com.vk.id.sample.xml.uikit.common.onVKIDAuthFail
+import com.vk.id.sample.xml.uikit.common.onVKIDAuthSuccess
 
 data class IconButtonItem(
     val style: OneTapStyle
--- sample/xml/src/main/java/com/vk/id/sample/xml/multibranding/MultibrandingXmlLayoutActivity.kt
+++ sample/xml/src/main/java/com/vk/id/sample/xml/multibranding/MultibrandingXmlLayoutActivity.kt
@@ -1,13 +1,33 @@
 package com.vk.id.sample.xml.multibranding
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.vk.id.multibranding.xml.OAuthListWidget
 import com.vk.id.sample.xml.R
+import com.vk.id.sample.xml.multibranding.util.getOAuthListCallback
+import com.vk.id.sample.xml.uikit.common.onVKIDAuthFail
 
 public class MultibrandingXmlLayoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.vkid_activity_multibranding)
+        forEachOAuthListWidget(findViewById<View>(android.R.id.content).rootView) { widget ->
+            widget.setCallbacks(
+                onAuth = getOAuthListCallback(this),
+                onFail = { onVKIDAuthFail(this, it) },
+            )
+        }
+    }
+
+    private fun forEachOAuthListWidget(view: View, action: (OAuthListWidget) -> Unit) {
+        if (view is OAuthListWidget) {
+            action(view)
+        }
+        if (view is ViewGroup) {
+            repeat(view.childCount) { forEachOAuthListWidget(view.getChildAt(it), action) }
+        }
     }
 }
