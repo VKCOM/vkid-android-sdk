5fc5428d35a463bafc173e79412e1b9b9f16f846
Sergey Golovin <s.golovin@vk.team>
2023-11-27 15:49:09 +0300

    Fix overlap with system bars

--- onetap-compose/src/main/kotlin/com/vk/id/onetap/compose/onetap/sheet/content/SheetContentBox.kt
+++ onetap-compose/src/main/kotlin/com/vk/id/onetap/compose/onetap/sheet/content/SheetContentBox.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -43,7 +43,7 @@ internal fun SheetContentBox(
 ) {
     Box(
         modifier = Modifier
-            .safeDrawingPadding()
+            .safeContentPadding()
             .wrapContentHeight()
             .fillMaxWidth()
             .widthIn(min = 344.dp, max = 800.dp)
