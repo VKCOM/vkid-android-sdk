dc9d862b49840ac273eece36bbad3b744ba43fd2
Daniil Klimchuk <daniil.klimchuk@vk.team>
2023-11-29 19:31:21 +0300

    VKIDSDK-492: Override equals instead of using data classes

--- vkid/src/main/java/com/vk/id/VKIDAuthFail.kt
+++ vkid/src/main/java/com/vk/id/VKIDAuthFail.kt
@@ -1,26 +1,72 @@
 package com.vk.id
 
-public sealed class VKIDAuthFail(public val description: String) {
-    public data class Canceled(
-        private val errorDescription: String
-    ) : VKIDAuthFail(errorDescription)
+public sealed class VKIDAuthFail(
+    public val description: String
+) {
+    public class Canceled(
+        description: String
+    ) : VKIDAuthFail(description) {
+        override fun equals(other: Any?): Boolean {
+            return other is Canceled && description == other.description
+        }
 
-    public data class FailedApiCall(
-        private val errorDescription: String,
+        override fun hashCode(): Int {
+            return description.hashCode()
+        }
+    }
+
+    public class FailedApiCall(
+        description: String,
         public val throwable: Throwable
-    ) : VKIDAuthFail(errorDescription)
+    ) : VKIDAuthFail(description) {
+        override fun equals(other: Any?): Boolean {
+            return other is FailedApiCall && description == other.description && throwable == other.throwable
+        }
 
-    public data class FailedOAuthState(
-        private val errorDescription: String
-    ) : VKIDAuthFail(errorDescription)
+        override fun hashCode(): Int {
+            var result = description.hashCode()
+            result = 31 * result + throwable.hashCode()
+            return result
+        }
+    }
 
-    public data class FailedRedirectActivity(
-        private val errorDescription: String,
+    public class FailedOAuthState(description: String) : VKIDAuthFail(description) {
+        override fun equals(other: Any?): Boolean {
+            return other is FailedOAuthState && description == other.description
+        }
+
+        override fun hashCode(): Int {
+            return description.hashCode()
+        }
+    }
+
+    public class FailedRedirectActivity(
+        description: String,
         public val throwable: Throwable?
-    ) : VKIDAuthFail(errorDescription)
+    ) : VKIDAuthFail(description) {
+        override fun equals(other: Any?): Boolean {
+            return other is FailedRedirectActivity && description == other.description && throwable == other.throwable
+        }
 
-    public data class NoBrowserAvailable(
-        private val errorDescription: String,
+        override fun hashCode(): Int {
+            var result = description.hashCode()
+            result = 31 * result + throwable.hashCode()
+            return result
+        }
+    }
+
+    public class NoBrowserAvailable(
+        description: String,
         public val throwable: Throwable?
-    ) : VKIDAuthFail(errorDescription)
+    ) : VKIDAuthFail(description) {
+        override fun equals(other: Any?): Boolean {
+            return other is NoBrowserAvailable && description == other.description && throwable == other.throwable
+        }
+
+        override fun hashCode(): Int {
+            var result = description.hashCode()
+            result = 31 * result + throwable.hashCode()
+            return result
+        }
+    }
 }
