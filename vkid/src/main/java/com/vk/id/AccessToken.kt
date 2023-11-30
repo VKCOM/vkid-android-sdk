a96f28474155fe8ff02bcfff8acff54002d2476a
Daniil Klimchuk <daniil.klimchuk@vk.team>
2023-11-30 16:23:39 +0300

    VKIDSDK-0: Generate equals and hashcode for tests

--- vkid/src/main/java/com/vk/id/AccessToken.kt
+++ vkid/src/main/java/com/vk/id/AccessToken.kt
@@ -9,4 +9,27 @@ public class AccessToken(
     public val userID: Long,
     public val expireTime: Long,
     public val userData: VKIDUser,
-) : Parcelable
+) : Parcelable {
+
+    override fun hashCode(): Int {
+        var result = token.hashCode()
+        result = 31 * result + userID.hashCode()
+        result = 31 * result + expireTime.hashCode()
+        result = 31 * result + userData.hashCode()
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AccessToken
+
+        if (token != other.token) return false
+        if (userID != other.userID) return false
+        if (expireTime != other.expireTime) return false
+        if (userData != other.userData) return false
+
+        return true
+    }
+}
--- vkid/src/main/java/com/vk/id/VKIDUser.kt
+++ vkid/src/main/java/com/vk/id/VKIDUser.kt
@@ -14,7 +14,36 @@ public class VKIDUser(
     public val photo100: String? = null,
     public val photo200: String? = null,
     public val email: String? = null
-) : Parcelable
+) : Parcelable {
+
+    override fun hashCode(): Int {
+        var result = firstName.hashCode()
+        result = 31 * result + lastName.hashCode()
+        result = 31 * result + (phone?.hashCode() ?: 0)
+        result = 31 * result + (photo50?.hashCode() ?: 0)
+        result = 31 * result + (photo100?.hashCode() ?: 0)
+        result = 31 * result + (photo200?.hashCode() ?: 0)
+        result = 31 * result + (email?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as VKIDUser
+
+        if (firstName != other.firstName) return false
+        if (lastName != other.lastName) return false
+        if (phone != other.phone) return false
+        if (photo50 != other.photo50) return false
+        if (photo100 != other.photo100) return false
+        if (photo200 != other.photo200) return false
+        if (email != other.email) return false
+
+        return true
+    }
+}
 
 internal fun SilentAuthInfo.toVKIDUser() = VKIDUser(
     firstName = firstName,
