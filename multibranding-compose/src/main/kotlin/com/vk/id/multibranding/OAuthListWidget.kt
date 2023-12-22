2c5ceb7260e8641cdd9d523409e498fa86951bf6
Daniil Klimchuk <daniil.klimchuk@vk.team>
2023-12-22 13:29:59 +0300

    VKIDSDK-475: Add another constructor for OAuthListWidget

--- multibranding-compose/src/main/kotlin/com/vk/id/multibranding/OAuthListWidget.kt
+++ multibranding-compose/src/main/kotlin/com/vk/id/multibranding/OAuthListWidget.kt
@@ -58,6 +58,33 @@ import kotlinx.coroutines.launch
  * @param onAuth A callback to be invoked upon a successful auth.
  * @param onFail A callback to be invoked upon an error during auth.
  * @param oAuths A set of [OAuth]s the should be displayed to the user.
+ */
+@Composable
+public fun OAuthListWidget(
+    modifier: Modifier = Modifier,
+    style: OAuthListWidgetStyle = OAuthListWidgetStyle.Dark(),
+    onAuth: OAuthListWidgetAuthCallback,
+    onFail: (OAuth, VKIDAuthFail) -> Unit,
+    oAuths: Set<OAuth> = OAuth.values().toSet(),
+) {
+    OAuthListWidget(
+        modifier = modifier,
+        style = style,
+        onAuth = onAuth,
+        onFail = onFail,
+        oAuths = oAuths,
+        vkid = null
+    )
+}
+
+/**
+ * Constructs a multibranding widget that supports auth with multiple [OAuth]s.
+ *
+ * @param modifier Layout configuration for the widget.
+ * @param style Styling widget configuration.
+ * @param onAuth A callback to be invoked upon a successful auth.
+ * @param onFail A callback to be invoked upon an error during auth.
+ * @param oAuths A set of [OAuth]s the should be displayed to the user.
  * @param vkid An optional [VKID] instance to use for authentication.
  *  If instance of VKID is not provided, it will be created on first composition.
  */
