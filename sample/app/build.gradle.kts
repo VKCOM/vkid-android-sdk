b6becda521c4e97685c4cfa82a1589faf28d9b30
Daniil Klimchuk <daniil.klimchuk@vk.team>
2024-01-05 20:07:47 +0300

    VKIDSDK-0: Enable R8

--- sample/app/build.gradle.kts
+++ sample/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             signingConfig = signingConfigs.findByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
