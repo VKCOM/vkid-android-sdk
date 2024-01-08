bfd54446079366ede0632f2cbb19780345c05590
Daniil Klimchuk <daniil.klimchuk@vk.team>
2024-01-08 23:23:33 +0300

    VKIDSDK-619: Move baseline profile to common module

--- multibranding-common/build.gradle.kts
+++ multibranding-common/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("vkid.android.library")
     id("vkid.android.publish")
     id("org.jetbrains.kotlinx.binary-compatibility-validator")
-    id("vkid.android.baseline.profile")
 }
 
 android {
