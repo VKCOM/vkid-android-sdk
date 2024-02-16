d18b55c35959a85f358afc04e88cec5ad145d939
Daniil Klimchuk <daniil.klimchuk@vk.team>
2024-02-16 19:24:08 +0300

    VKIDSDK-824: Remove lateinit vars

--- sdk/core/vkid/src/main/java/com/vk/id/internal/api/sslpinning/api/DefaultSSLTrustManagerProvider.kt
+++ sdk/core/vkid/src/main/java/com/vk/id/internal/api/sslpinning/api/DefaultSSLTrustManagerProvider.kt
@@ -5,19 +5,13 @@ import com.vk.id.internal.api.sslpinning.okhttp.security.SSLCertificateStore
 import com.vk.id.internal.api.sslpinning.okhttp.trust.SSLTrustManager
 
 internal class DefaultSSLTrustManagerProvider(
-    private val context: Context
+    private val context: Context,
+    private val certificateStore: SSLCertificateStore,
+    private val reuseSslSocketFactory: Boolean = false,
 ) {
-
-    private lateinit var certificateStore: SSLCertificateStore
-    private var reuseSslSocketFactory = false
 
     private val lazyTrustManager by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         SSLTrustManager(context, certificateStore)
-    }
-
-    fun init(store: SSLCertificateStore, reuseSslSocketFactory: Boolean) {
-        this.certificateStore = store
-        this.reuseSslSocketFactory = reuseSslSocketFactory
     }
 
     fun provideManager(): SSLTrustManager =
