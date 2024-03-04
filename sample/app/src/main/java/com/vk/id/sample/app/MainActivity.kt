e81cc1d5d3f4082a4de84f727ed8a487be791561
Daniil Klimchuk <daniil.klimchuk@vk.team>
2024-03-04 20:44:19 +0300

    VKIDSDK-799: Add bottom navigation to sample

--- sample/app/src/main/java/com/vk/id/sample/app/MainActivity.kt
+++ sample/app/src/main/java/com/vk/id/sample/app/MainActivity.kt
@@ -5,17 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.activity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.vk.id.VKID
+import com.vk.id.sample.app.screen.bottomnav.BottomNavigation
+import com.vk.id.sample.app.screen.bottomnav.Screen
 import com.vk.id.sample.app.screen.home.HomeScreen
 import com.vk.id.sample.app.screen.multibranding.MultibrandingComposeScreen
 import com.vk.id.sample.app.screen.sheet.OneTapBottomSheetScreen
 import com.vk.id.sample.app.screen.styling.OnetapStylingComposeScreen
+import com.vk.id.sample.app.screen.utils.UtilsScreen
 import com.vk.id.sample.app.uikit.theme.AppTheme
 import com.vk.id.sample.xml.multibranding.MultibrandingXmlLayoutActivity
 import com.vk.id.sample.xml.onetap.OneTapStylingXmlLayoutActivity
@@ -30,17 +35,22 @@ internal class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             AppTheme {
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                    NavHost(
-                        navController = navController,
-                        startDestination = "home"
-                    ) {
-                        composable("home") { HomeScreen(navController) }
-                        composable("onetap-styling-compose") { OnetapStylingComposeScreen() }
-                        activity("onetap-styling-xml-layout") { activityClass = OneTapStylingXmlLayoutActivity::class }
-                        composable("multibranding-compose") { MultibrandingComposeScreen() }
-                        activity("multibranding-xml-layout") { activityClass = MultibrandingXmlLayoutActivity::class }
-                        composable("onetap-bottom-sheet") { OneTapBottomSheetScreen() }
-                        activity("onetap-bottom-sheet-xml") { activityClass = OnetapSheetStylingXmlActivity::class }
+                    BottomNavigation(navController = navController) { innerPadding ->
+                        NavHost(
+                            navController = navController,
+                            startDestination = Screen.Main.route,
+                            Modifier.padding(innerPadding),
+                        ) {
+                            composable(Screen.Main.route) { HomeScreen(navController) }
+                            composable(Screen.Utils.route) { UtilsScreen() }
+
+                            composable("onetap-styling-compose") { OnetapStylingComposeScreen() }
+                            activity("onetap-styling-xml-layout") { activityClass = OneTapStylingXmlLayoutActivity::class }
+                            composable("multibranding-compose") { MultibrandingComposeScreen() }
+                            activity("multibranding-xml-layout") { activityClass = MultibrandingXmlLayoutActivity::class }
+                            composable("onetap-bottom-sheet") { OneTapBottomSheetScreen() }
+                            activity("onetap-bottom-sheet-xml") { activityClass = OnetapSheetStylingXmlActivity::class }
+                        }
                     }
                 }
             }
---
+++ sample/app/src/main/java/com/vk/id/sample/app/screen/utils/UtilsScreen.kt
@@ -0,0 +1,9 @@
+package com.vk.id.sample.app.screen.utils
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun UtilsScreen() {
+    Text(text = "Utils")
+}
