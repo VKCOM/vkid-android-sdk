c826e2dd16fcbb8d0324b15a93c4f55491ce4a2d
Sergey Golovin <s.golovin@vk.team>
2024-03-07 14:27:45 +0300

    Rename LogTracker -> LogcatTracker

--- sdk/core/analytics/src/main/kotlin/com/vk/id/analytics/LogcatTracker.kt
+++ sdk/core/analytics/src/main/kotlin/com/vk/id/analytics/LogcatTracker.kt
@@ -7,7 +7,7 @@ import com.vk.id.common.InternalVKIDApi
  * Tracker for [VKIDAnalytics] that track events to Android logcat
  */
 @InternalVKIDApi
-public class LogTracker(private val tag: String = "VKID Analytics") : VKIDAnalytics.Tracker {
+public class LogcatTracker(private val tag: String = "VKID Analytics") : VKIDAnalytics.Tracker {
     /** Log event to logcat with format 'event name' { param1name: param1value, ... } **/
     override fun trackEvent(name: String, vararg params: VKIDAnalytics.EventParam) {
         var paramsString = "{ "
