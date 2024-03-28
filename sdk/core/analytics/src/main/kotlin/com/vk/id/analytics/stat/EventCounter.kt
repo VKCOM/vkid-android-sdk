a989c9292a50daefc3590a6d97bb757ebd1caab5
Sergey Golovin <s.golovin@vk.team>
2024-03-28 18:32:38 +0300

    Synchronize EventCounter

--- sdk/core/analytics/src/main/kotlin/com/vk/id/analytics/stat/EventCounter.kt
+++ sdk/core/analytics/src/main/kotlin/com/vk/id/analytics/stat/EventCounter.kt
@@ -4,11 +4,16 @@ import java.util.concurrent.TimeUnit
 
 internal class EventCounter {
     private val startId = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()).toInt()
+
+    @Volatile
     var prevEventId = 0
         private set
+
+    @Volatile
     var eventId = startId
         private set
 
+    @Synchronized
     fun increment() {
         prevEventId = eventId
         eventId++
