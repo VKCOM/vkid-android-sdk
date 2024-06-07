08fbb48502c8e7276f9cdc2acb10f3936b788692
Daniil Klimchuk <daniil.klimchuk@vk.team>
2024-06-07 17:57:13 +0300

    VKIDSDK-0: Fix git names validation and health metrics

--- build-logic/metrics/src/main/kotlin/com/vk/id/health/metrics/git/Git.kt
+++ build-logic/metrics/src/main/kotlin/com/vk/id/health/metrics/git/Git.kt
@@ -6,6 +6,8 @@ internal object Git {
     val currentCommitHash get() = exec("git rev-parse --verify HEAD")
 
     fun getRootCommitHash(sourceBranch: String, targetBranch: String): String {
+        runCatching { exec("git fetch origin $sourceBranch:$sourceBranch") }
+        runCatching { exec("git fetch origin $targetBranch:$targetBranch") }
         val mergeBase = exec("git merge-base $sourceBranch $targetBranch")
         return exec("git rev-list --no-merges -n 1 $mergeBase")
     }
@@ -13,10 +15,12 @@ internal object Git {
     private fun exec(command: String): String {
         val process = Runtime.getRuntime().exec(command)
         val output = StringBuilder()
+        val error = StringBuilder()
         process.inputReader().lines().forEach(output::append)
+        process.errorReader().lines().forEach { error.append("$it\n") }
         val exitCode = process.waitFor()
         if (exitCode != 0) {
-            throw IOException("Command exited with $exitCode")
+            throw IOException("Command exited with $exitCode\n$error")
         }
         return output.toString()
     }
