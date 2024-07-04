fdce82997c3c55c0f0010e18b042f5e648b2a025
Daniil Klimchuk <daniil.klimchuk@vk.team>
2024-07-04 15:07:10 +0300

    VKIDSDK-1313: Allow to calculate averate build speed

--- build-logic/metrics/src/main/kotlin/com/vk/id/health/metrics/buildspeed/BuildSpeedRepository.kt
+++ build-logic/metrics/src/main/kotlin/com/vk/id/health/metrics/buildspeed/BuildSpeedRepository.kt
@@ -6,11 +6,6 @@ internal class BuildSpeedRepository(
     measuredTaskPaths: Set<String>,
 ) {
 
-    private companion object {
-        private const val FIELD_BUILD_DURATION = "BUILD_DURATION_MS"
-        private const val FIELD_CONFIGURATION_DURATION = "CONFIGURATION_DURATION_MS"
-    }
-
     private val storage = FirestoreMetricStorage(
         metricName = "Build speed",
         metricCollectionName = "build-speed-metrics",
@@ -18,21 +13,51 @@ internal class BuildSpeedRepository(
         documentSuffix = measuredTaskPaths.joinToString(),
     )
 
+    fun initMetrics(iterations: Int) {
+        (1..iterations)
+            .flatMap { listOf(getBuildDurationField(it) to -1, getConfigurationDurationField(it) to -1) }
+            .toMap()
+            .let(storage::saveMetrics)
+    }
+
     internal fun saveBuildDuration(
+        iteration: Int,
         buildDuration: Long,
         configurationDuration: Long,
-    ) = storage.saveMetrics(
+    ) = storage.updateMetrics(
         mapOf(
-            FIELD_BUILD_DURATION to buildDuration,
-            FIELD_CONFIGURATION_DURATION to configurationDuration,
+            getBuildDurationField(iteration) to buildDuration,
+            getConfigurationDurationField(iteration) to configurationDuration,
         )
     )
 
-    internal fun getBuildDuration() = storage.getMetric<Long>(FIELD_BUILD_DURATION)?.takeIf { it != 0L } ?: 1L
+    internal fun getSourceBuildDuration(iterations: Int): Long {
+        return getBuildDuration(iterations, ::getBuildDurationField) { storage.getSourceMetric<Long>(it) }
+    }
 
-    internal fun getConfigurationDuration() = storage.getMetric<Long>(FIELD_CONFIGURATION_DURATION)?.takeIf { it != 0L } ?: 1L
+    internal fun getTargetBuildDuration(iterations: Int): Long {
+        return getBuildDuration(iterations, ::getBuildDurationField) { storage.getTargetMetric<Long>(it) }
+    }
 
-    internal fun saveDiff(diff: String) = storage.saveDiff(diff)
+    internal fun getSourceConfigurationDuration(iterations: Int): Long {
+        return getBuildDuration(iterations, ::getConfigurationDurationField) { storage.getSourceMetric<Long>(it) }
+    }
 
-    internal fun getDiff() = storage.getDiff()
+    internal fun getTargetConfigurationDuration(iterations: Int): Long {
+        return getBuildDuration(iterations, ::getConfigurationDurationField) { storage.getTargetMetric<Long>(it) }
+    }
+
+    private fun getBuildDuration(
+        iterations: Int,
+        fieldAccessor: (iteration: Int) -> String,
+        metricAccessor: (field: String) -> Long?
+    ): Long {
+        return (1..iterations)
+            .map { fieldAccessor(it) }
+            .sumOf { metricAccessor(it)?.takeIf { it != 0L } ?: 1L }
+            .div(iterations)
+    }
+
+    private fun getBuildDurationField(iteration: Int) = "BUILD_DURATION_MS_$iteration"
+    private fun getConfigurationDurationField(iteration: Int) = "CONFIGURATION_DURATION_MS_$iteration"
 }
