7894c9b3f823017762f22154c3cc91d59f695d81
Daniil Klimchuk <daniil.klimchuk@vk.team>
2024-08-22 16:35:43 +0300

    VKIDSDK-1419: Add release sample assemble integration

--- scripts/test/sample-assembling-integration.sh
+++ scripts/test/sample-assembling-integration.sh
@@ -8,6 +8,12 @@
 # - runs ./gradlew allDependencies --write-locks
 # - builds sample
 # - undoes all the changes
+#
+# Contract:
+# The first parameter is test mode
+# "./sample-assembling-integration 0" Tests integration with vk sdk
+# "./sample-assembling-integration 1" Tests plain sample assembling with sdk from maven local
+# "./sample-assembling-integration 2" Tests sample assembling with sdk from artifactory
 
 set -e
 
@@ -25,16 +31,26 @@ replaceGradleProperty() {
     fi
 }
 
+MODE=$1
+
 importCommon
 CURRENT_VERSION="$(fetchCurrentVersion)"
 NEW_VERSION="$CURRENT_VERSION-SNAPSHOT"
-bumpVersionInVersionFile "$CURRENT_VERSION" "$NEW_VERSION"
-./gradlew publishToMavenLocal
+if [ "$MODE" -ne 2 ]; then
+    bumpVersionInVersionFile "$CURRENT_VERSION" "$NEW_VERSION"
+    ./gradlew publishToMavenLocal
+fi
 replaceGradleProperty "s/SUBSTITUTE_SAMPLE_PROJECTS_WITH_MODULES=false/SUBSTITUTE_SAMPLE_PROJECTS_WITH_MODULES=true/"
-replaceGradleProperty "s/DEPEND_ON_VK_SDK=false/DEPEND_ON_VK_SDK=true/"
+if [ "$MODE" -eq 0 ]; then
+    replaceGradleProperty "s/DEPEND_ON_VK_SDK=false/DEPEND_ON_VK_SDK=true/"
+fi
 ./gradlew allDependencies --write-locks
 ./gradlew :sample-app:assembleDebug
-bumpVersionInVersionFile "$NEW_VERSION" "$CURRENT_VERSION"
-replaceGradleProperty "s/SUBSTITUTE_SAMPLE_PROJECTS_WITH_MODULES=true/SUBSTITUTE_SAMPLE_PROJECTS_WITH_MODULES=false/"
-replaceGradleProperty "s/DEPEND_ON_VK_SDK=true/DEPEND_ON_VK_SDK=false/"
+if [ "$MODE" -ne 2 ]; then
+    bumpVersionInVersionFile "$NEW_VERSION" "$CURRENT_VERSION"
+    replaceGradleProperty "s/SUBSTITUTE_SAMPLE_PROJECTS_WITH_MODULES=true/SUBSTITUTE_SAMPLE_PROJECTS_WITH_MODULES=false/"
+fi
+if [ "$MODE" -eq 0 ]; then
+    replaceGradleProperty "s/DEPEND_ON_VK_SDK=true/DEPEND_ON_VK_SDK=false/"
+fi
 ./gradlew allDependencies --write-locks
