d23f7e12fe004f5bfa06566f547eccf77f23a320
Sergey Golovin <s.golovin@vk.team>
2024-08-28 23:33:23 +0300

    VKIDSDK-1441: Remove startActivity mock

--- sdk/ui-tests/src/androidTest/kotlin/com/vk/id/common/UiTestApplication.kt
+++ sdk/ui-tests/src/androidTest/kotlin/com/vk/id/common/UiTestApplication.kt
@@ -1,25 +1,11 @@
 package com.vk.id.common
 
 import android.app.Application
-import android.content.Intent
 import android.content.pm.PackageManager
 
 public class UiTestApplication : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-    }
-
     var mockPackageManager: PackageManager? = null
-    var mockStartActivity: ((Intent) -> Unit)? = null
 
     override fun getPackageManager(): PackageManager = mockPackageManager ?: super.getPackageManager()
-
-    override fun startActivity(intent: Intent) {
-        if (mockStartActivity == null) {
-            super.startActivity(intent)
-        } else {
-            mockStartActivity?.invoke(intent)
-        }
-    }
 }
--- sdk/ui-tests/src/androidTest/kotlin/com/vk/id/common/activity/AutoTestActivity.kt
+++ sdk/ui-tests/src/androidTest/kotlin/com/vk/id/common/activity/AutoTestActivity.kt
@@ -1,6 +1,5 @@
 package com.vk.id.common.activity
 
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Handler
 import android.os.Looper
@@ -12,15 +11,6 @@ public class AutoTestActivity : ComponentActivity() {
     public fun setContent(view: View) {
         Handler(Looper.getMainLooper()).post {
             setContentView(view)
-        }
-    }
-
-    override fun startActivity(intent: Intent) {
-        val mockStartActivity = (this.application as? UiTestApplication)?.mockStartActivity
-        if (mockStartActivity == null) {
-            super.startActivity(intent)
-        } else {
-            mockStartActivity.invoke(intent)
         }
     }
 
